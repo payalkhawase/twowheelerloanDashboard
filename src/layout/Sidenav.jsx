9f2c08414877e4b512f208e808158fe3842a35f8
sujata-cell <srjp90@gmail.com>
2025-05-05 19:56:48 +0530

    1st add

---
+++ src/layout/Sidenav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sidenav() {
+  return (
+    <div style={{backgroundColor:'darkslategray',width:"20%",height:"600px"}} class="h-100 d-inline-block">Sidenav</div>
+  )
+} 
+
+export default Sidenav
