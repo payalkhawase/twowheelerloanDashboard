44c3bce427b269f7fa7eed6fe54853c056edc9bf
GajananChavhan-pixel <shubhamchavhan077@gmail.com>
2025-05-15 10:10:07 +0530

    Clear the conflicts

--- src/layout/Sidenav.jsx
+++ src/layout/Sidenav.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-// import { Collapse } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
 import { Collapse } from "react-bootstrap";
 
